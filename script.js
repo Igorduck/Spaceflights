5e8958427f7f2605d75a7ade8e2ad943905548ec
Igorduck <72630374+Igorduck@users.noreply.github.com>
2020-12-06 22:53:20 -0300

    Site Final

---
+++ script.js
@@ -0,0 +1,12 @@
+//ano atual
+const x=2020;
+
+//ano de nascimento
+const y = prompt("Em que ano vc nasceu?")
+
+const nome = prompt("Qual é o seu nome?")
+
+let z;
+z= (x-y)
+
+alert(`Olá ${nome} seja bem vindo(a) ao meu site! Pelos meus cálculos você tem ${z} anos.`)
